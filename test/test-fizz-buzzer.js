09ce4126c6ca40370a78dac1e9b1cf58a1464e33
Alex Widner <manny1806@gmail.com>
2018-05-25 10:18:21 -0700

    added chai

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -4,10 +4,21 @@ const num = require('../fizzBuzzer.js')
 describe ('num' , function(){
   it('should be divisible by 15,5,3 and be a number.', function(){
 
-  //expect(num('the')).to.equal(Error(`'num' must be a number`));
-  expect(num(15)).to.equal('fizz-buzz');
-  expect(num(3)).to.equal('fizz');
-  expect(num(5)).to.equal('buzz');
-  expect(num(7)).to.equal(7);
+  const testCases = [['the',`Error: 'num' must be a number`],
+                     [15, 'fizz-buzz'], 
+                     [3, 'fizz'], 
+                     [5, 'buzz'],
+                     [7, 7]];
+  for (let x = 0; x < testCases.length; x++){
+    if(isNaN(testCases[x][0])){
+      expect(function() {num(testCases[x][0])}).to.throw(Error);
+    } else {
+      expect(num(testCases[x][0])).to.equal(testCases[x][1]);
+    }
+  }
+  // expect(num(15)).to.equal('fizz-buzz');
+  // expect(num(3)).to.equal('fizz');
+  // expect(num(5)).to.equal('buzz');
+  // expect(num(7)).to.equal(7);
   });
 });
